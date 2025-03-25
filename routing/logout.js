0e6967cac542c0b7821557bf56405c400afbf925
dima-kharchenko <dmitriy020606@gmail.com>
2025-03-25 20:50:04 +0100

    Minor changes

--- routing/logout.js
+++ routing/logout.js
@@ -5,6 +5,20 @@
 // Podpowiedź: return response.end();
 const logoutRouting = (method, response) => {
     response.setHeader("Content-Type", "text/html") 
+    response.write(`
+        <html>
+        <head>
+            <title>Shop - Logout</title>
+        </head>
+        <body>
+            <h1>Logout</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/kill">Logout from application</a>
+            </nav>
+        </body>
+        </html>
+        `)
     response.end() 
 }
 
